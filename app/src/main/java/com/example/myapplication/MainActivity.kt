ca54685e46c9b8aac649d64c4dc43619b7b93c08
Alexander Nesterov <41343903+AlexanderNest@users.noreply.github.com>
2020-12-27 01:35:14 +0300

    Есть все, кроме правильного перехода: - не продолжается генерация элементов с того же места

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import android.content.res.Configuration
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.Animation.AnimationListener
@@ -13,25 +14,25 @@ import androidx.recyclerview.widget.GridLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
 
+
 class MainActivity : AppCompatActivity() {
 
-    var data = mutableListOf<String>()
+    var data = arrayListOf<String>()
     var currentId = 0
-    var poolOfRemoved = mutableListOf<String>()
+    var poolOfRemoved = arrayListOf<String>()
+    lateinit var cor : Job
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE){
-
             recyclerView.layoutManager = GridLayoutManager(this, 4)
         }
         else{
             recyclerView.layoutManager = GridLayoutManager(this, 2)
         }
-
 
         recyclerView.adapter = CustomRecyclerAdapter(data)
 
@@ -39,18 +40,21 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun fillList(): List<String> {
-        (0..10).forEach { i -> data.add("$currentId")
-                currentId++
-        }
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
 
-        return data
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
     }
 
     private fun autoAdd(){
-        GlobalScope.launch{
+        cor = GlobalScope.launch{
             while (true){
-                val index = (0..data.size).random()
+                val index = data.size//(0..data.size).random()
+                Log.i("currentid", currentId.toString())
 
                 Thread(Runnable {
                     this@MainActivity.runOnUiThread(java.lang.Runnable {
@@ -72,15 +76,11 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
     fun onRemoveButtonClicked(view: View){
         var parentlayout = view.parent as LinearLayout
         var id = (parentlayout.getChildAt(0) as TextView).text
 
         val anim = AnimationUtils.loadAnimation(this, R.anim.remove)
-        //view.startAnimation(anim)
-
-        //val anim = AnimationUtils.makeOutAnimation(this, true)
 
         anim.setAnimationListener(object : AnimationListener {
             override fun onAnimationStart(animation: Animation) {
@@ -95,43 +95,5 @@ class MainActivity : AppCompatActivity() {
             override fun onAnimationRepeat(animation: Animation) {}
         })
         view.startAnimation(anim)
-
-
-
-
-        /*val animator = ValueAnimator.ofFloat(0f, 100f)
-        animator.duration = 1000
-        animator.start()
-
-        animator.addUpdateListener(object : ValueAnimator.AnimatorUpdateListener {
-            override fun onAnimationUpdate(animation: ValueAnimator) {
-                val animatedValue = animation.animatedValue as Float
-                parentlayout.translationX = animatedValue
-            }
-        })*/
-
-
-        //data.remove(id)
-        /*Thread(Runnable {
-            // performing some dummy time taking operation
-            var i=0;
-            while(i<Int.MAX_VALUE){
-                i++
-            }
-
-            // try to touch View of UI thread
-            this@MainActivity.runOnUiThread(java.lang.Runnable {
-                data.add("$currentId")
-                currentId++
-                recyclerView.adapter?.notifyDataSetChanged()
-            })
-        }).start()*/
-
-
     }
-
-
-
-
-
 }
