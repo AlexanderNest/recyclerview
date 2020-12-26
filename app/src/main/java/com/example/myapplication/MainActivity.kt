1314528fdf6ef029b7fe3d228d4fa2491786f5fa
Alexander Nesterov <41343903+AlexanderNest@users.noreply.github.com>
2020-12-26 23:59:20 +0300

    Добавлено автоматическое добавление элементов

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,6 +6,7 @@ import android.animation.ValueAnimator
 import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Contacts
 import android.view.View
 import android.view.animation.AccelerateDecelerateInterpolator
 import android.view.animation.Animation
@@ -18,17 +19,21 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 import org.w3c.dom.Text
+import kotlinx.coroutines.*
+import java.lang.Thread.sleep
 
+import kotlin.coroutines.coroutineContext
 
 class MainActivity : AppCompatActivity() {
 
     var data = mutableListOf<String>()
-
+    var currentId = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         fillList()
+
 
         if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE){
 
@@ -40,12 +45,38 @@ class MainActivity : AppCompatActivity() {
 
 
         recyclerView.adapter = CustomRecyclerAdapter(data)
+
+        autoAdd()
+
     }
 
     private fun fillList(): List<String> {
-        (0..10).forEach { i -> data.add("$i") }
+        (0..10).forEach {
+                i -> data.add("$currentId")
+                currentId++
+        }
+
         return data
     }
+
+    fun autoAdd(){
+        GlobalScope.launch{
+            while (true){
+                Thread(Runnable {
+                    this@MainActivity.runOnUiThread(java.lang.Runnable {
+
+                        data.add("$currentId")
+                        currentId++
+                        recyclerView.adapter?.notifyDataSetChanged()
+                    })
+                }).start()
+
+                delay(5000)
+            }
+
+        }
+    }
+
 
     fun onRemoveButtonClicked(view : View){
         var parentlayout = view.parent as LinearLayout
@@ -53,6 +84,11 @@ class MainActivity : AppCompatActivity() {
 
         val anim = AnimationUtils.loadAnimation(this, R.anim.remove)
         view.startAnimation(anim)
+
+        data.remove(id)
+        recyclerView.adapter?.notifyDataSetChanged()
+
+
         /*val animator = ValueAnimator.ofFloat(0f, 100f)
         animator.duration = 1000
         animator.start()
@@ -66,10 +102,26 @@ class MainActivity : AppCompatActivity() {
 
 
         //data.remove(id)
+        /*Thread(Runnable {
+            // performing some dummy time taking operation
+            var i=0;
+            while(i<Int.MAX_VALUE){
+                i++
+            }
+
+            // try to touch View of UI thread
+            this@MainActivity.runOnUiThread(java.lang.Runnable {
+                data.add("$currentId")
+                currentId++
+                recyclerView.adapter?.notifyDataSetChanged()
+            })
+        }).start()*/
 
 
-        recyclerView.adapter?.notifyDataSetChanged()
     }
+
+
+
 
 
 }
